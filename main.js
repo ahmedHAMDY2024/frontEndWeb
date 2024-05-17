2d4f4f0272217f38046ed2ed9d90ba0a75223d4e
ahmedfrontendX <a01022395595@gmail.com>
2024-05-18 00:09:14 +0300

    its divolbment code

--- main.js
+++ main.js
@@ -8,15 +8,25 @@ let boat=document.getElementById("boat")
 
 onscroll=function (){
     let value=scrollY
-    if(value<=150){
+    if(value<=120){
     star.style.left=value+"px"
-    star.style.width=this.width-value+"px"
-    moon.style.top=value*2+"px"
+    star.style.width-value+"px"
+    moon.style.top=value*3+"px"
     text.style.fontSize=value+"px"
-    boat.style.left=value*5+"px"
-    boat.style.top=value+"px"
+    boat.style.left=value*2+"px"
+    boat.style.top=value*2+"px"
     mountain2.style.top=value+"px"
+    mountain2.style.right=value+"px"
+    mountain1.style.top=value+"px"
+    mountain1.style.left=value+"px"
     river.style.top=value+"px"
+    if(value>=115){
+        star.style.display="none"
+        boat.style.display="none"
+    }else{
+        star.style.display="block"
+        boat.style.display="block"
+    }
 }
 
     
