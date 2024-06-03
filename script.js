41060712df20caabe2cde4ba36cfdc10b7bac860
ahmedhamdy <a01022395595@gmail.com>
2024-06-03 23:37:44 +0300

    creat frontEndWeb

---
+++ script.js
@@ -0,0 +1,34 @@
+// let star: string = document.getElementById("stars");
+// let moon: string = document.getElementById("moon");
+// let text = document.getElementById("text");
+// let mountain1 = document.getElementById("mountain1");
+// let mountain2 = document.getElementById("mountain2");
+// let river = document.getElementById("river");
+// let boat = document.getElementById("boat");
+// let x: number = 8;
+// onscroll = function () {
+//   let value = scrollY;
+//   if (value <= 120) {
+//     star.style.left = value + "px";
+//     star.style.width - value + "px";
+//     moon.style.top = value * 3 + "px";
+//     text.style.fontSize = value + "px";
+//     boat.style.left = value * 2 + "px";
+//     boat.style.top = value * 2 + "px";
+//     // mountain2.style.top=value+"px"
+//     // mountain2.style.right=value+"px"
+//     // mountain1.style.top=value+"px"
+//     // mountain1.style.left=value+"px"
+//     // river.style.top=value+"px"
+//     if (value >= 115) {
+//       star.style.display = "none";
+//       boat.style.display = "none";
+//     } else {
+//       star.style.display = "block";
+//       boat.style.display = "block";
+//     }
+//   }
+// };
+var t = "55";
+var x = "55";
+console.log(t + "\n" + x);
